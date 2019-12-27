9f9ab654d7ba5365dec399842483930ef90325d5
fox <artemdav2@gmail.com>
2019-12-27 13:56:23 +0600

    Обновление модели
    Добавлен вывод вюхи

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,17 @@
+"""""
+path("", views.MoviesView.as_view()),
+
+path("<int:pk>/", views.MovieDetaiView.as_view(),  name="movie_detail")
+
+path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail")
+"""
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.MoviesView.as_view())
+    path("", views.MoviesView.as_view()),
+
+    path("<slug:slug>/", views.MovieDetaiView.as_view(),  name="movie_detail")
+
 ]
