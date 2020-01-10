634d90ace07d85f26ce4bc1a26c3f62b487599a9
fox1985 <artemdav2gmail.com>
2020-01-10 21:22:15 +0600

    Дороботка админки

--- movies/admin.py
+++ movies/admin.py
@@ -2,11 +2,64 @@ from django.contrib import admin
 from .models import  Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(MovieShots)
-admin.site.register(Actor)
-admin.site.register(Rating)
-admin.site.register(RatingStar)
-admin.site.register(Reviews)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категории"""
+    list_display = ('id', 'name', 'url')
+    list_display_links = ("name",)
+
+
+class ReviewsInline(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ('name', 'email')
+
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    """Фильм"""
+    list_display = ('title', 'category', 'url', 'draft')
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'category__name',)
+    inlines = [ReviewsInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ('draft',)
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    """Отзывы"""
+    list_display = ( 'name', 'email', 'text', 'parent', 'movie', 'id' )
+    readonly_fields = ('name', 'email',)
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Жанры"""
+    list_display = ('name', 'description', 'url', )
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    """Кадры из фильма"""
+    list_display = ('title', 'description', 'image', 'movie', )
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    """Актеры и режиссеры"""
+    list_display = ('name', 'age', 'description', 'image', )
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """Рейтинг"""
+    list_display = ('ip', 'star', 'movie', )
+
+@admin.register(RatingStar)
+class RatingStarAdmin(admin.ModelAdmin):
+    """Звезда рейтинга"""
+    list_display = ('value', )
+
+
+
+
+
+
