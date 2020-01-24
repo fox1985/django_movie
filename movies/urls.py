4961b6eaaadaa6d96c4a43e9d19c69681c0d20e4
fox1985 <artemdav2gmail.com>
2020-01-24 14:19:46 +0600

    Дороботка админки Добавил django-ckeditor 5.8.0  и плагин ютуб Добавил сесии
    Выводит категории через тэг

--- movies/urls.py
+++ movies/urls.py
@@ -10,10 +10,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.MoviesView.as_view()),
 
-    path("<slug:slug>/", views.MovieDetaiView.as_view(),  name="movie_detail"),
-
-    path("review/<int:pk>/", views.AddReview.as_view(),  name="add_review"),
+    path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
+    path("review/<int:pk>/", views.AddReview.as_view(), name="add_review"),
+    path("", views.MoviesView.as_view()), # чтобы выводила на главную траницу
 
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
-from .models import Movie
+from .models import Movie, Category
 from  .forms import ReviewsForm
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """Список фильтерв"""
-    def get(self, request):
-        movies = Movie.objects.filter(draft=False)
-        return render(request, "movies/movie_list.html", {"movie_list": movies})
+    model = Movie
+    queryset =  Movie.objects.filter(draft=False)
 
 
 
-class MovieDetaiView(View):
+class MovieDetailView(DetailView):
     """Полное описание фильма"""
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, "movies/movie_detail.html", {'movie': movie})
+    model = Movie
+    slug_field = "url"
+
+
 
 
 class AddReview(View):
