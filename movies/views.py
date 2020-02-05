97acea5b419a2c3ca1ed7b352eeb4b89fa584edf
fox1985 <artemdav2gmail.com>
2020-02-05 21:52:44 +0600

    Дороботка админки Добавил django-ckeditor 5.8.0  и плагин ютуб Добавил сесии
    Выводит категории через тэг
    
    Вывод ПОСЛЕДНИЕ ДОБАВЛЕННЫЕ
    
    Вывод актеров и режисеров
    
    фильтры фильмоф

--- movies/views.py
+++ movies/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
-from .models import Movie, Actor
+from .models import Movie, Actor, Genre
 from  .forms import ReviewsForm
+from django.db.models import Q
 
 
-class MoviesView(ListView):
+class GenreYear:
+    """Жанры и года выхода фильмов"""
+    def get_genres(self):
+        return  Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+
+class MoviesView(GenreYear, ListView):
     """Список фильтерв"""
     model = Movie
     queryset =  Movie.objects.filter(draft=False)
 
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = "url"
@@ -34,8 +45,18 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     """Вывод информации о актере"""
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = 'name'
+
+
+class FilterMoviesView(GenreYear, ListView):
+    """Фильтр фильмов"""
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(genres__in=self.request.GET.getlist("genre"))
+        )
+        return queryset
